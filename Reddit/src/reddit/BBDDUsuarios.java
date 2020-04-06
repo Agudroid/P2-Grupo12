3e9e4f7e53e6f3019b8546560ac5d3e8b0f69f70
Vichicius <adrianlopezcouso@gmail.com>
2020-04-06 19:59:57 +0200

    Prueba

--- Reddit/src/reddit/BBDDUsuarios.java
+++ Reddit/src/reddit/BBDDUsuarios.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -22,15 +23,23 @@ import java.util.List;
 public class BBDDUsuarios implements Serializable{
     
     private List <Usuarios> ListaUsuarios = new LinkedList<>();
-
+    public void añadirUsuario(Usuarios u){
+    ListaUsuarios.add(u);
+    }
     public BBDDUsuarios(List<Usuarios> Usuarios) {
         this.ListaUsuarios=Usuarios;
     }
    
     public void CargarBBDD(File f) throws IOException{
-        
+       try{ 
         ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
-        
+        List <Usuarios> ListaUsuariosRecuperado = (List <Usuarios>) ois.readObject();
+       for (Usuarios ListaUsuarios : ListaUsuariosRecuperado){
+            System.out.println("ListaUsuarios");
+       }
+       }catch(Exception e){
+           
+       }  
     }
     
     public void EscribirBBDD(File f)throws IOException{
