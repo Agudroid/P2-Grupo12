9c7fdf11896cf28e1abb352949a45ca3674e4f64
Agudroid <60368201+Agudroid@users.noreply.github.com>
2020-04-06 18:32:29 +0200

    Add files via upload

---
+++ PracticaMP/src/BBDDUsuarios.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica.mp2;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Administrador
+ */
+public class BBDDUsuarios implements Serializable{
+    
+    
+    
+    
+    
+}
