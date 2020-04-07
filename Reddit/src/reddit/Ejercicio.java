0cf90cc4eb1c2260754bb7a68fa26646b850bba4
Alba-18 <albasevi8@gmail.com>
2020-04-07 18:59:16 +0200

    actualizado_3

--- Reddit/src/reddit/Ejercicio.java
+++ Reddit/src/reddit/Ejercicio.java
@@ -15,16 +15,49 @@ public abstract class Ejercicio extends EntradaGenerica {
     private int puntuacion;
     private String titulo;
     private String texto;
-    boolean verificada;
+    private boolean verificada;
     
+    @Override
+    public String getTitulo(){
+        return titulo;
+    }
+    
+    @Override
+    public String getTexto(){
+        return texto;
+    }
+    
+    @Override
+    public void setTitulo(String titulo){
+        this.titulo= titulo;
+    }
+    
+    @Override
+    public void setTexto(String texto){
+        this.texto= texto;
+    }
+    
+    @Override
     public int getPuntuacion(){
         return puntuacion;
     }
     
+    @Override
+    public boolean getVerificada(){
+        return verificada;
+    }
+    
+    @Override
     public void verificar(boolean resultado){
         this.verificada= resultado;
     }
     
-    public boolean comentar (String texto);
-    public abstract boolean votar (int valor);
+    @Override
+    public abstract boolean comentar (String texto);
+    
+    @Override
+    public boolean votar (int valor){
+        this.puntuacion= puntuacion+valor;
+        return true;
+    }
 }
