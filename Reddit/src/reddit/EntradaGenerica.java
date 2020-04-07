b4091d0f54804e05d694a1b638bcf78804e426a9
miri-fa <miriamdefco@gmail.com>
2020-04-07 18:01:23 +0200

    miriam

---
+++ Reddit/src/reddit/EntradaGenerica.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reddit;
+
+/**
+ *
+ * @author miriamdefrancisco
+ */
+public abstract class EntradaGenerica {
+    private int puntuacion;
+    private String titulo;
+    private String texto;
+    boolean verificada;
+    
+    public abstract int getPuntuacion();
+    public abstract void verificar(boolean resultado);
+    public abstract boolean comentar (String texto);
+    public abstract boolean votar (int valor);
+    
+}
