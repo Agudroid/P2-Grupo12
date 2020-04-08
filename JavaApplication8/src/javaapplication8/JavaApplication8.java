c9596d3fb64173c8f32aef7bd05e36c65822d835
Vichicius <adrianlopezcouso@gmail.com>
2020-04-08 20:23:22 +0200

    Prueba

---
+++ JavaApplication8/src/javaapplication8/JavaApplication8.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication8;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author adri-
+ */
+public class JavaApplication8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Calendar FechaIni = Calendar.getInstance();
+        FechaIni.add(Calendar.DAY_OF_MONTH, 2);
+        System.out.println("La fecha del fin de la penalizacion es: "+FechaIni.getTime());
+    }
+    public int setFechaDePenalizacion (){
+        int FechaFin = FechaIni.;
+        return FechaFin;
+        
+    }
+    
+}
