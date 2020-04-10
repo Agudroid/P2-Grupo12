a5ebd0171ec0256eada491b08a1285a54b811a6c
miri-fa <miriamdefco@gmail.com>
2020-04-10 18:18:32 +0200

    resetear foro

--- Reddit/src/reddit/Foro.java
+++ Reddit/src/reddit/Foro.java
@@ -15,34 +15,7 @@ import java.util.Scanner;
  */
 class Foro {
     
-    private List <Usuarios> ListaUsuarios=new LinkedList<>();
-    private List <SubForo> ListaSubForos=new LinkedList<>();
-    
-    public void Welcome(){
-        
-        System.out.println("Bienvenido al Foro");
-        System.out.println("1.Iniciar sesion sin registro");
-        System.out.println("2.Iniciar sesion registrado");
-        System.out.println("3.Registrarse");
-        Scanner Sc = new Scanner(System.in);
-        
-        int opcion = Sc.nextInt();
-        
-        switch (opcion) {
-            
-            case 1:
-                System.out.println("Bienvenido ");
-                
-            break;
-            
-            case 2:
-                System.out.println("Introduce tu correo");
-                String correo=Sc.nextLine();
-                System.out.println("Introduce tu contraseña");
-                String contraseña=Sc.nextLine();
-                System.out.println("Patata");
-                
-                
+    private List <Usuarios> ListaUsuarios=new LinkedList<>();       
             
         }
         
