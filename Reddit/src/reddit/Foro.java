0dc7dd298b7386427773857826dda9a7bf709681
Vichicius <adrianlopezcouso@gmail.com>
2020-04-10 19:04:25 +0200

    Prueba

--- Reddit/src/reddit/Foro.java
+++ Reddit/src/reddit/Foro.java
@@ -5,6 +5,13 @@
  */
 package reddit;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
@@ -16,6 +23,7 @@ import java.util.Scanner;
 public class Foro {
     private static Foro instancia;
     private  String nombre;
+    private List <Usuarios> ListaUsuarios = new LinkedList<>();
     
     private Foro(String nombre){
         this.nombre = nombre;
@@ -27,6 +35,58 @@ public class Foro {
         }
         return instancia;
     }
+    
+        public boolean login (String contraseña, String correo){
+        boolean verificado = false;
+        Iterator <Usuarios> it = ListaUsuarios.iterator() ;
+           while(it.hasNext() && verificado==false){
+           Usuarios usuarioActual = it.next();
+               if (usuarioActual.getCorreo().equals(correo) && usuarioActual.getContraseña().equals(contraseña) ) {
+                 verificado = true;
+                   System.out.println("Es correcto.");
+               }
+
+           }
+           if (verificado==false) {
+                   System.out.println("Has escrito mas el correo o la contraseña. Por favor, intentelo de nuevo");
+                   
+           }
+        return verificado;
+        }
+
+        public void añadirLista(Usuarios u){
+            ListaUsuarios.add(u);
+        }
+
+        public void CargarBBDD(File f) throws IOException, ClassNotFoundException{
+            try{
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
+
+            Usuarios aux = (Usuarios) ois.readObject();
+            System.out.println(aux.getNombre());
+            System.out.println(aux.getApellido());
+            System.out.println(aux.getCorreo());
+            System.out.println(aux.getNick());
+            System.out.println(aux.getContraseña());
+            System.out.println("");
+            }catch(IOException e){
+                System.out.println(e.getMessage()); 
+            }
+        }
+
+        public void EscribirBBDD(File f)throws IOException{
+
+            try{
+               ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f));
+               Iterator <Usuarios> it = ListaUsuarios.iterator() ;
+               while(it.hasNext()){
+               Usuarios usuarioActual = it.next();
+               oos.writeObject(usuarioActual);
+               }
+            }catch(IOException e){
+                System.out.println(e.getMessage()); 
+            }
+        }
 }            
 
         
