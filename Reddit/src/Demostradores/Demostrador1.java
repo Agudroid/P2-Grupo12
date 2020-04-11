a8c07d44c270d06c213d4e0ff9232ec566436f63
miri-fa <miriamdefco@gmail.com>
2020-04-11 17:37:17 +0200

    Demostrador 1 modificacion

--- Reddit/src/Demostradores/Demostrador1.java
+++ Reddit/src/Demostradores/Demostrador1.java
@@ -7,20 +7,27 @@ package Demostradores;
 import reddit.Foro;
 /**
  *
+ * 
  * @author miriamdefrancisco
  */
 public class Demostrador1 {
+    /**Este primer demostrador sirve para añadir contenido a las bases de datos,
+     primero intentando iniciar sesión de manera fallida y luego creando un usuario
+     registrándolo en la base de datos, después, creamos un subforo y una entrada*/
     public static void main(String[] args) {
             System.out.println("Inicializamos el foro:");
             Foro sistema = Foro.getForo("Reddit URJC");
             System.out.println("El foro ha sido creado.");
-            System.out.println("Iniciamos sesión en el sistema con un usuario registrado");
-            boolean encontrado = sistema.login("12345", "m.d.2018@alumnos.urjc.es");
+            System.out.println("Iniciamos sesión en el sistema con un usuario no registrado");
+            boolean encontrado = sistema.login("12345", "m.defrancisco.2018@alumnos.urjc.es");
             if (encontrado){
                 System.out.println("Bienvenido al foro");
             } else {
                 System.out.println("Error, la contraseña y/o correo no coinciden");
             }
-            System.out.println("Creamos un subforo");
+            System.out.println("Vamos a registrarnos en el sistema");
+            sistema.Registrarse("Miriam","de Francisco","m.defrancisco.2018@alumnos.urjc.es","miri.fa","12345");
+               
+            
     }   
 }
