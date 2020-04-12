30e24a2e4f756652b7ff5973ee8da06ce871a068
miri-fa <miriamdefco@gmail.com>
2020-04-12 13:33:50 +0200

    D2

--- Reddit/src/Demostradores/Demostrador2.java
+++ Reddit/src/Demostradores/Demostrador2.java
@@ -16,10 +16,10 @@ public class Demostrador2 {
      comentamos y votamos una entrada ya existente de manera correcta*/
     public static void main(String[] args){
             System.out.println("Inicializamos el foro:");
-            Foro sistema = Foro.getForo("Reddit URJC");
+            Foro sistema = Foro.GetForo("Reddit URJC");
             System.out.println("El foro ha sido creado.");
             System.out.println("Iniciamos sesión en el sistema con un usuario registrado");
-            boolean encontrado = sistema.login("12345", "m.d.2018@alumnos.urjc.es");
+            boolean encontrado = sistema.Login("12345", "m.d.2018@alumnos.urjc.es");
             if (encontrado){
                 System.out.println("Bienvenido al foro");
             } else {
