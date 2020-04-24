87c651a04a97f947df58b2d8dcb3e949ceb5e928
Vichicius <adrianlopezcouso@gmail.com>
2020-04-24 18:59:08 +0200

    Prueba

--- Reddit/src/Demostradores/Demostrador1.java
+++ Reddit/src/Demostradores/Demostrador1.java
@@ -24,13 +24,14 @@ public class Demostrador1 {
             System.out.println("El foro ha sido creado.");
             System.out.println("Iniciamos sesión en el sistema con un usuario no registrado");
             boolean encontrado = sistema.Login("12345", "m.defrancisco.2018@alumnos.urjc.es");
-            if (encontrado){
-                System.out.println("Bienvenido al foro");
-            } else {
+            if (!encontrado){
                 System.out.println("Error, la contraseña y/o correo no coinciden");
-            }
-            System.out.println("Vamos a registrarnos en el sistema");
-            sistema.RegistrarUsuario("Miriam","de Francisco","m.defrancisco.2018@alumnos.urjc.es","miri.fa","12345");
+                System.out.println("Vamos a registrarnos en el sistema");
+                boolean esRegistrado = sistema.RegistrarUsuario("Miriam","de Francisco","m.defrancisco.2018@alumnos.urjc.es","miri.fa","12345", "12345");
+                if(esRegistrado == true){
+                    System.out.println("Se ha registrado con existo");
+                }
+            } 
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(Demostrador1.class.getName()).log(Level.SEVERE, null, ex);
         }
