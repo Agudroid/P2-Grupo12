ab637da12eb56e3283c2e90ff4bdf823a59add9e
Vichicius <adrianlopezcouso@gmail.com>
2020-04-25 19:42:35 +0200

    Prueba

--- Reddit/src/reddit/Foro.java
+++ Reddit/src/reddit/Foro.java
@@ -67,23 +67,23 @@ public class Foro {
         String prueba;
         boolean resultado;
         Scanner sc = new Scanner(correo);
-        prueba = sc.findInLine("([a-z]).([a-z]+)20([0-9][0-9])@alumnos.urjc.es");
-        String prueba2 = sc.findInLine("([a-z]).([a-z]+)@urjc.es");
-        boolean alumno = correo.equals(prueba);
-        boolean profesor = correo.equals(prueba2);
-        resultado = false;
-        if (alumno==true || profesor ==true){
-           if (contraseña1.equals(contraseña2)){
-            Usuarios usuario = new Usuarios(nombre,apellido,correo,nick,contraseña1);
-            ListaUsuarios.add(usuario); 
-            resultado = true;
+        prueba = sc.findInLine("([a-z]).([a-z]+)20([0-9][0-9])@alumnos.urjc.es"); //comprobamos que es el patron del correo del alumno
+        String prueba2 = sc.findInLine("([a-z]).([a-z]+)@urjc.es"); //comprobamos que es el patron del correo del profesor
+        boolean alumno = correo.equals(prueba); //comprobamos que es el correo del alumno
+        boolean profesor = correo.equals(prueba2); //comprobamos que es el correo del alumno
+        resultado = false; //inicializamos la var resultado
+        if (alumno==true || profesor ==true){ //si el correo coincide tanto para alumno o profesor entramos en el if
+           if (contraseña1.equals(contraseña2)){ //si al registrarse las contraseñas coinciden entras al if
+            Usuarios usuario = new Usuarios(nombre,apellido,correo,nick,contraseña1); //te has registrado con exito
+            ListaUsuarios.add(usuario); //Se añade a la lista de usuarios registrados
+            resultado = true; // resultado pasa a valer true porque se ha registrado con exito
            }
            else{
-               System.out.println("Las contraseñas no coinciden. Por favor intentelo de nuevo.");  
+               System.out.println("Las contraseñas no coinciden. Por favor intentelo de nuevo."); //las contraseñas no coinciden  
            }
         }
         else{
-           System.out.println("Has escrito mal el correo. Por favor intentelo de nuevo.");         
+           System.out.println("Has escrito mal el correo. Por favor intentelo de nuevo.");  //el correo no coincide con el de profesor o alumno        
            
         }
         return resultado;
@@ -91,22 +91,21 @@ public class Foro {
         
     public boolean Logout (){ //método que permitirá salir de la aplicación, cerrar sesión
         Verificado = false;
-        return Verificado;
+        return Verificado; // siempre va a devolver false porque haces logout
         // Despues de esto llamar a los metodos login o registrarse
     }
     
     public boolean CrearSubForo(String nombre) throws IOException{ //permitirá crear un nuevo subforo, que se añadirá al foro
-        SubForo f = new SubForo(nombre);
-        ListaSubForo.add(f);
-        foro.EscribirBBDD();
-        return ListaSubForo.contains(f);
+        SubForo f = new SubForo(nombre); //creamos un subforo y le pasamos su nombre
+        ListaSubForo.add(f); //se añade a la lista de subforors
+        foro.EscribirBBDD(); //lo guardamos en la base de datos
+        return ListaSubForo.contains(f); //devolvemos true si se ha guardado con exito y false en caso contrario
     }
     
     public void CargarBBDD() throws ClassNotFoundException, IOException{
         
         if(f.exists()){
-            
-        
+
             try{
                 ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
                 SubForo s = (SubForo) ois.readObject();
@@ -125,10 +124,10 @@ public class Foro {
     public void EscribirBBDD()throws IOException{
         try{
             ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f));
-            Iterator <SubForo> it = ListaSubForo.iterator() ;
-            while(it.hasNext()){
-                SubForo subForoActual = it.next();
-                oos.writeObject(subForoActual);
+            Iterator <SubForo> it = ListaSubForo.iterator() ; //creamos el iterador para recorrer la lista
+            while(it.hasNext()){ //no salimos del bucle hasta recorrer por completo la lista
+                SubForo subForoActual = it.next(); // asignamos a la var subForoActual el subforo actual de la lista que estamos recorriendo
+                oos.writeObject(subForoActual); //Escribimos el subforo
             }
         }
         catch(IOException e){
