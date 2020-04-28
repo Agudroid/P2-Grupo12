5fa43219a649d47eec11d68054d03a921dbadb59
Alba-18 <albasevi8@gmail.com>
2020-04-28 17:49:58 +0200

    demostradores

--- Reddit/src/Demostradores/Demostrador2.java
+++ Reddit/src/Demostradores/Demostrador2.java
@@ -1,44 +1,45 @@
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package Demostradores;
+
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import reddit.Foro;
+
 /**
  *
- * 
- * @author miriamdefrancisco
+ * @author Alba Sevillano
  */
-public class Demostrador2 {
-    /**En este demostrador iniciamos sesión con un alumno no registrado y veremos 
+public class Demostrador2 {  //HECHO
+
+    /**En este demostrador iniciamos sesión con un profesor no registrado y veremos 
      que da error, intentaremos registrarlo pero no se podrá porque los datos son 
-     erroneos (ALUMNO)*/
+     erroneos (PROFESOR)*/
     public static void main(String[] args) throws IOException {
         try {
             System.out.println("Inicializamos el foro:");
             Foro sistema = Foro.GetForo("Reddit URJC");
             System.out.println("El foro ha sido creado.");
             System.out.println("Iniciamos sesión en el sistema con un usuario no registrado");
-            boolean encontrado = sistema.Login("12345", "m.defrancisco.2018@alumnos.urjc.es");
+            boolean encontrado = sistema.Login("67890", "a.perez@urjc.es");
             if (!encontrado){
                 System.out.println("Error, la contraseña y/o correo no coinciden");
                 System.out.println("Vamos a registrarnos en el sistema");
-                boolean esRegistrado = sistema.RegistrarUsuario("Miriam","de Francisco","m.defrancisco.2018@alumnos.urjc.com","miri.fa","12345", "12345");
+                boolean esRegistrado = sistema.RegistrarUsuario("Antonio","Perez","a.perez@urjc.es","a.perez","67890", "67890");
                 if(esRegistrado == false){
                     System.out.println("No se ha podido registrar.");
                 }
             }
 
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Demostrador1.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Demostrador4.class.getName()).log(Level.SEVERE, null, ex);
         }
                
             
-    }   
+    }
+    
 }
-
--- Reddit/src/Demostradores/Demostrador4.java
+++ Reddit/src/Demostradores/Demostrador4.java
@@ -4,41 +4,40 @@
  * and open the template in the editor.
  */
 package Demostradores;
-
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import reddit.Foro;
-
 /**
  *
- * @author adri-
+ * 
+ * @author miriamdefrancisco
  */
 public class Demostrador4 {
-
     /**Este primer demostrador sirve para añadir contenido a las bases de datos,
      primero intentando iniciar sesión de manera fallida y luego creando un usuario
-     profesor registrándolo en la base de datos, después podremos añadir una entrada 
-     a un subforo (PROFESOR)*/
-    public static void main(String[] args) throws IOException{
+     alumno registrándolo en la base de datos, después podremos ver una entrada y 
+     comentarla (ALUMNO)*/
+    public static void main(String[] args) throws IOException {
         try {
+            
             System.out.println("Inicializamos el foro:");
             Foro sistema = Foro.GetForo("Reddit URJC");
             System.out.println("El foro ha sido creado.");
-            System.out.println("Iniciamos sesión en el sistema con un usuario registrado");
-            boolean encontrado = sistema.Login("67890", "a.perez@urjc.es");
+            System.out.println("Iniciamos sesión en el sistema con un usuario no registrado");
+            boolean encontrado = sistema.Login("12345", "m.defrancisco.2018@alumnos.urjc.es");
             if (!encontrado){
                 System.out.println("Error, la contraseña y/o correo no coinciden");
                 System.out.println("Vamos a registrarnos en el sistema");
-                boolean esRegistrado = sistema.RegistrarUsuario("Antonio","Perez","a.perez@urjc.es","a.perez","67890", "67890");
+                boolean esRegistrado = sistema.RegistrarUsuario("Miriam","de Francisco","m.defrancisco.2018@alumnos.urjc.es","miri.fa","12345", "12345");
                 if(esRegistrado == true){
                     System.out.println("Se ha registrado con existo");
                 }
-            }
-        } 
-        catch (ClassNotFoundException ex) {
-            Logger.getLogger(Demostrador2.class.getName()).log(Level.SEVERE, null, ex);
+            } 
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Demostrador4.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
-    
+               
+            
+    }   
 }
