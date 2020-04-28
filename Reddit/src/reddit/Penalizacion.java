a50d65e5a4091c6d58909524176a6b0ecbe8431d
miri-fa <miriamdefco@gmail.com>
2020-04-28 20:27:15 +0200

    penalizacion

--- Reddit/src/reddit/Penalizacion.java
+++ Reddit/src/reddit/Penalizacion.java
@@ -1,41 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package reddit;
 
+
 import java.io.Serializable;
-import java.util.Calendar;
 
-/**
- *
- * @author Administrador
- */
+
 public class Penalizacion implements Serializable{
-    private Calendar FechaIni;
-    private Calendar FechaFin;
 
-    public Penalizacion(Calendar FechaIni, Calendar FechaFin) {
-        this.FechaIni = FechaIni;
-        this.FechaFin = FechaFin;
-    }
-
-    public Calendar getFechaIni() {
-        return FechaIni;
-    }
-
-    public Calendar getFechaFin() {
-        return FechaFin;
-    }
-
-    public void setFechaIni(Calendar FechaIni) {
-        this.FechaIni = FechaIni;
-    }
-
-    public void setFechaFin(Calendar FechaFin) {
-        this.FechaFin = FechaFin;
-    }
-    
-    
+	private boolean activa;
+	private int diasPenalizacion;
+	
+	public Penalizacion(int dias) {
+		diasPenalizacion = dias;
+		activa = true;
+	}
+	
+	public boolean estaActiva() {
+		return activa;
+	}
+	
+	public void avanzarDias(int dias) {
+		diasPenalizacion -= dias;
+		if(diasPenalizacion < 0) {
+			diasPenalizacion = 0;
+			activa = false;
+		}
+	}
 }
