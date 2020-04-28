06d785bc4377d3ea1e567778f70cf6a548497879
miri-fa <miriamdefco@gmail.com>
2020-04-28 17:59:40 +0200

    serializable

--- Reddit/src/reddit/Administrador.java
+++ Reddit/src/reddit/Administrador.java
@@ -5,11 +5,13 @@
  */
 package reddit;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Administrador
  */
-public class Administrador extends Usuarios {
+public class Administrador extends Usuarios implements Serializable{
 
     public Administrador(String Nombre, String Apellido, String Correo, String Nick, String Contraseña) {
         super(Nombre, Apellido, Correo, Nick, Contraseña);
