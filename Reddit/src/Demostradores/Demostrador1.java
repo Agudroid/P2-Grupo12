b5cad578b3fa55fdda1d4b2e250680587191b33a
Alba-18 <albasevi8@gmail.com>
2020-04-29 13:24:53 +0200

    Demostrador 1

--- Reddit/src/Demostradores/Demostrador1.java
+++ Reddit/src/Demostradores/Demostrador1.java
@@ -22,45 +22,73 @@ public class Demostrador1 {
        y añadir una entrada (PROFESOR)*/
     public static void main(String[] args) throws IOException{
         try {
-            System.out.println("Inicializamos el foro:");
+            System.out.println("Inicializamos el foro Reddit URJC");
             Foro sistema = Foro.GetForo("Reddit URJC");
-            System.out.println("El foro ha sido creado.");
-            System.out.println("Iniciamos sesión en el sistema con un usuario registrado");
+            System.out.println("");
+            
+            System.out.println("El foro ha sido creado con exito");
+            System.out.println("Iniciamos sesión en el sistema");
             boolean encontrado = sistema.Login("67890", "a.perez@urjc.es");
             if (!encontrado){
+                System.out.println(" ");
                 System.out.println("Error, la contraseña y/o correo no coinciden");
+                System.out.println(" ");
                 System.out.println("Vamos a registrarnos en el sistema");
                 boolean esRegistrado = sistema.RegistrarUsuario("Antonio","Perez","a.perez@urjc.es","a.perez","67890", "67890");
                 if(esRegistrado == true){
-                    System.out.println("Se ha registrado con existo");
-                    
+                    System.out.println("a.perez se ha registrado con exito");
+                    System.out.println(" ");
                 }
             }
       
             System.out.println("Ahora intentamos iniciar sesion con el usuario que"
-                    + "acabamos de crear");
+                    + "acabamos de registrar");
             encontrado =sistema.Login("67890", "a.perez@urjc.es");
             if(encontrado){
-                System.out.println("Hemos encontrado el usuario");
-                boolean SubForoCreado=sistema.CrearSubForo("SubForo de prueba");
+                System.out.println(" ");
+                System.out.println("El usuario se encuentra en la base de datos");
+                System.out.println(" ");
+                System.out.println("Voy a crear un subforo");
+                boolean SubForoCreado=sistema.CrearSubForo("SubForo MP");
                 if(SubForoCreado){
-                    System.out.println("SubForo creado");
-                    SubForo SubForoActual = sistema.verSubForo("SubForo de prueba");
-                    Entrada entrada = SubForoActual.CrearEntrada("Entrada de prueba");
-                    TextoPlano txt = new TextoPlano("Introducción","patata");
-                    entrada.AñadirComponente(txt);
+                    System.out.println("El SubForo ha sido creado con éxito");
+                    SubForo SubForoReddit = sistema.verSubForo("SubForo MP");
+                    
+                    System.out.println(" ");
+                    
+                    Entrada entrada = SubForoReddit.CrearEntrada("Introducción");
+                    TextoPlano Entrada_txt = new TextoPlano("Explicación","El siguiente subforo"
+                            +"se corresponde con la asignatura de MP");
+                    entrada.AñadirComponente(Entrada_txt);
                     Administrador admin = new Administrador("Pedro", "Garcia", "p.garcia@urjc.es", "p.garcia", "cc987");
                     admin.verificarEntrada(entrada,true);
-                    System.out.println("Creamos una entrada al subForo");
-                    Encuesta encuesta = new Encuesta("Los ovnis existen?","Crees que existen los ovnis");
-                    encuesta.SetRespuesta("Sí");
-                    encuesta.SetRespuesta("No");
-                    System.out.println("Añadimos a la Entrada una encuesta");
-                    entrada.AñadirComponente(encuesta);
-                    admin.verificarEntrada(entrada,true);                  
+                    
+                    System.out.println("Se ha creado una entrada con texto plano, que se ha añadido"
+                            + "al SubForo Mp");
+                    Encuesta Entrada_encuesta = new Encuesta("ENCUESTA ENTREGA PRÁCTICA","¿Te parece bien la entrega de la práctica?");
+                    Entrada_encuesta.SetRespuesta("SÍ");
+                    Entrada_encuesta.SetRespuesta("NO");
+                    
+                    System.out.println(" ");
+                    
+                    entrada.AñadirComponente(Entrada_encuesta);
+                    admin.verificarEntrada(entrada,true);   
+                    System.out.println("Hemos verificado la entrada, tras haberle añadido la encuesta");
+                    
+                    System.out.println(" ");
+                    System.out.println("La entrada añadida es:");
+                    System.out.println(entrada.GetTitulo());
+                    System.out.println(Entrada_txt.GetTitulo());
+                    System.out.println(Entrada_txt.GetTexto());
+                    System.out.println(Entrada_encuesta.GetTitulo());
+                    System.out.println(Entrada_encuesta.GetTexto());
+                    System.out.println(Entrada_encuesta.GetRespuestas());
                 }
             }
-            sistema.Logout();
+            boolean logout = sistema.Logout();
+            if(logout==false){
+                System.out.println("Has cerrado sesion. Hasta pronto");                
+            }
         } 
         catch (ClassNotFoundException ex) {
             Logger.getLogger(Demostrador5.class.getName()).log(Level.SEVERE, null, ex);
