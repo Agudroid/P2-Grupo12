796af7e4fc114b982608dd21013ce57522c3ea90
Vichicius <adrianlopezcouso@gmail.com>
2020-04-29 13:28:31 +0200

    Prueba

--- Reddit/src/Demostradores/Demostrador6.java
+++ Reddit/src/Demostradores/Demostrador6.java
@@ -28,27 +28,27 @@ public class Demostrador6 {
         try {
             System.out.println("Inicializamos el foro:");
             Foro sistema = Foro.GetForo("Reddit URJC");
-            System.out.println("El foro ha sido creado.");
+            System.out.println("El foro ha sido creado."); //se ha cargado el Foro con exito
             System.out.println("Iniciamos sesión en el sistema con un usuario registrado");
-            boolean encontrado = sistema.Login("12345", "m.defrancisco.2018@alumnos.urjc.es");
-            if (encontrado){
+            boolean encontrado = sistema.Login("12345", "m.defrancisco.2018@alumnos.urjc.es"); // si el usuario esta registrado y escribe bien el correo y la contraseña se guarda true
+            if (encontrado){ // existe el usuario
                 System.out.println("Bienvenido al foro");
-                SubForo sf  = sistema.verSubForo("SubForo de prueba");
-                sf.AñadirSuscriptor();
-                Entrada e = sf.CrearEntrada("Prueba1");
-                Administrador admin = new Administrador("Pedro", "Garcia", "p.garcia@urjc.es", "p.garcia", "cc987");
-                admin.verificarEntrada(e, true);
-                Ejercicio en = new Ejercicio("Ejercicio", "1+1 ", "= 2");
-                boolean posible = e.AñadirComponente(en);
+                SubForo sf  = sistema.verSubForo("SubForo MP"); //accedemos al subforo "SubForo MP"
+                sf.AñadirSuscriptor(); // el usuario loggeado se suscribe al subforo
+                Entrada e = sf.CrearEntrada("Ejercicio de Matemáticas"); // creamos una entrada con su titulo
+                Administrador admin = new Administrador("Pedro", "Garcia", "p.garcia@urjc.es", "p.garcia", "cc987"); //creamos un administrador para verificar la entrada
+                admin.verificarEntrada(e, true); // El administrador acepta la entrada
+                Ejercicio en = new Ejercicio("Ejercicio", "1+1 ", "= 2"); // Introducimos el titulo, el enunciado y la solucion  
+                boolean posible = e.AñadirComponente(en); //si tienes autorizacion para añadir el componente, se guarda true, en caso contrario se guarda false
                 if (!posible){
-                    System.out.println("No puedes añadir un ejercicio siendo un alumno");
+                    System.out.println("No puedes añadir un ejercicio siendo un alumno."); //solo los profesores pueden crear ejercicios
                 }
-                Entrada e2 = sf.verEntrada("Segunda entrada");
+                Entrada e2 = sf.verEntrada("Segunda entrada"); //accedemos a "Segunda entrada" 
                 e2.Votar(1);
-                System.out.println("La entrada tiene de valoración: "+e2.GetPuntuacion());                
+                System.out.println("La entrada tiene de valoración: "+e2.GetPuntuacion()); //mostramos la valoracion de "Segunda entrada"               
             }
             boolean logout = sistema.Logout();
-            if(logout==false){
+            if(logout==false){ // si en la var logout se guarda un false, es decir, se hace logout correctamente, entra en el if
                 System.out.println("Has cerrado sesion. Hasta pronto");                
             }
         } catch (ClassNotFoundException ex) {
